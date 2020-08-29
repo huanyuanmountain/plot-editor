d27a6ae67cf0ab03154f985f6fc5caf6cb093d5c
noname <yeunghuanyuan@qq.com>
2020-08-29 23:32:08 +0800

    update dialog edit

--- src/components/DialogEdit.vue
+++ src/components/DialogEdit.vue
@@ -4,18 +4,22 @@
     <el-container>
       <el-aside>
         <el-row>
-          <el-col :span="5">
-            <el-button
-              @click="handleAddPlot"
-              type="primary"
-              icon="el-icon-circle-plus"
-              size="mini"
-            >增加剧情</el-button>
-          </el-col>
+          <!-- <el-col :span="5"> -->
+          <el-button
+            id="plot-id-tag"
+            @click="handleAddPlot"
+            type="primary"
+            icon="el-icon-circle-plus"
+            size="mini"
+          >增加剧情</el-button>
+          <!-- </el-col> -->
         </el-row>
-        <el-row>
+
+        <br />
+
+        <el-row class="search-layer">
           <el-col :span="6">
-            <div>搜索</div>
+            <div id="search-title">搜索:</div>
           </el-col>
 
           <el-col :span="18">
@@ -26,7 +30,13 @@
         <br />
 
         <el-row v-for="(id, index) in idList" :key="id + index">
-          <el-tag size="medium" effect="plain">{{ id + ' 事件' }}</el-tag>
+          <el-tag
+            id="plot-id-tag"
+            size="medium"
+            effect="plain"
+            @close="handleRemovePlot(id)"
+            closable
+          >{{ '剧情 '+ id }}</el-tag>
         </el-row>
       </el-aside>
     </el-container>
@@ -56,18 +66,40 @@ export default {
   },
 
   mounted() {
-
+    this.refreshIdList()
   },
 
   methods: {
-    ...mapMutations(['addPlot']),
+    ...mapMutations(['addPlot', 'removePlot']),
+
     handleAddPlot() {
       this.addPlot()
+      this.refreshIdList()
+    },
+
+    handleRemovePlot(id) {
+      this.removePlot(id)
+      this.refreshIdList()
+    },
+
+    refreshIdList() {
       this.idList = Object.keys(this.plot)
     }
+
   }
 }
 </script>
   
 <style>
+#plot-id-tag {
+  width: 80%;
+  margin-top: 6px;
+}
+.search-layer {
+  margin: 5px;
+}
+#search-title {
+  margin-top: 5px;
+  font-size: 13px;
+}
 </style>
